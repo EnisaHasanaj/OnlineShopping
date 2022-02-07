9bedfd771202a39b99ce1dbbb350859c9ea7ec9c
Eronita <eberisha@thequantix.com>
2022-02-07 14:19:24 +0100

    Added the new paths

--- src/router/index.js
+++ src/router/index.js
@@ -33,11 +33,14 @@ const routes = [
     component: Register
   },
   {
+    path: "/menu",
+    name: "menu",
+    component: Menu
+  },
+  {
     path: "/about",
     name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
+   
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue")
   }

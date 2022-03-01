c4650e51862802e75afd0884b00c91471156d785
Enise Hasanaj <eh47364@ubt-uni.net>
2022-03-01 23:06:07 +0100

    section of copyright

---
+++ front/src/components/Copyright.vue
@@ -0,0 +1,27 @@
+<template>
+
+  <!-- copyright section -->
+  <section id="copyright">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12 col-sm-12">
+          <h3>GAMING STORES</h3>
+          <p>Copyright © GAMING STORES</p>
+
+
+        </div>
+      </div>
+    </div>
+  </section>
+
+</template>
+
+<script>
+export default {
+name: "Copyright"
+}
+</script>
+
+<style scoped>
+
+</style>
